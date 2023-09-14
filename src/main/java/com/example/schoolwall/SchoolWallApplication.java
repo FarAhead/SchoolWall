a1fb35a320be3e176154f186deac111fbb0d32b6
murphy <2320086031@qq.com>
2023-09-14 22:29:54 +0800

    添加mybatis依赖并配置

--- src/main/java/com/example/schoolwall/SchoolWallApplication.java
+++ src/main/java/com/example/schoolwall/SchoolWallApplication.java
@@ -1,9 +1,11 @@
 package com.example.schoolwall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.demo.mapper")      //用于扫描Mapper包
 public class SchoolWallApplication {
 
 	public static void main(String[] args) {
