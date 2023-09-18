a05a56f8d620eb7815b3749e80e7273167b35ccd
yelir <3143334009@qq.com>
2023-09-18 17:10:14 +0800

    初步定义了相关类，并完成了若干基础操作

--- src/main/java/com/example/schoolwall/SchoolWallApplication.java
+++ src/main/java/com/example/schoolwall/SchoolWallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.demo.mapper")      //用于扫描Mapper包
+@MapperScan("com.example.schoolwall.mapper")      //用于扫描Mapper包
 public class SchoolWallApplication {
 
 	public static void main(String[] args) {
